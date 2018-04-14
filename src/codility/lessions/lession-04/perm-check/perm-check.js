5e86d28443e638911f7aab7d8069b0693cc7385e
somallg <tq.duong@icloud.com>
2018-04-15 00:07:39 +0700

    feat(codility): add lession 04: perm-check

---
+++ src/codility/lessions/lession-04/perm-check/perm-check.js
@@ -0,0 +1,12 @@
+/**
+ * @url https://www.hackerrank.com/challenges/perm-check/problem
+ */
+
+function solvePermCheck(a) {
+  var n = a.length;
+
+  var tmp = Array(n).fill(0);
+  return tmp.map((_, i) => ++tmp[a[i] - 1]).every(e => e === 1) ? 1 : 0;
+}
+
+module.exports = solvePermCheck;
