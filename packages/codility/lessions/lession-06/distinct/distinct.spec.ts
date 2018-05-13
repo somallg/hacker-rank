2b2cead973d61a85afbae396a58ab5bb0c20b9b3
somallg <tq.duong@icloud.com>
2018-05-13 11:03:32 +0700

    fix(ut): fix ut for distinct

--- packages/codility/lessions/lession-06/distinct/distinct.spec.ts
+++ packages/codility/lessions/lession-06/distinct/distinct.spec.ts
@@ -10,8 +10,16 @@ describe('Distinct', () => {
       expect(solveDistinct([2, 1, 1, 2, 3, 1])).toEqual(3);
     });
 
-    it('should return 3 when input ', () => {
+    it('should return 1 when input ', () => {
       expect(solveDistinct([1, 1, 1, 1, 1, 1])).toEqual(1);
+    });
+
+    it('should return 2 when input ', () => {
+      expect(solveDistinct([1, 1, 1, 1, 1, 2])).toEqual(2);
+    });
+
+    it('should return 2 when input ', () => {
+      expect(solveDistinct([1, 1, 1, 1, 1, 2, 3, 4])).toEqual(4);
     });
   });
 });
