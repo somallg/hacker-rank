08a599626d0722446670f1eb36ce51a8e8ae08ed
somallg <tq.duong@icloud.com>
2018-05-31 17:16:16 +0700

    feat(build): pump lint-staged version to work with node v10

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-((typescript-mode .
-   ((eval . (progn
-              (projectile-register-project-type 'yarn '("package.json")
-                :compile (format "yarn compile %s" (file-name-base buffer-file-name))
-                :test (format "yarn test:f %s" (file-name-base buffer-file-name))
-                :run (format "yarn debug %s" (file-name-base buffer-file-name))
-                :test-suffix ".spec"))))))
