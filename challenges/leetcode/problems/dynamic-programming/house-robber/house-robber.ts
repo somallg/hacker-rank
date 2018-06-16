ccfecefb97f2fee768b45739b3dedd08eca89be6
somallg <tq.duong@icloud.com>
2018-06-16 23:33:18 +0700

    feat(leetcode): solved house-robber bottom up

--- challenges/leetcode/problems/dynamic-programming/house-robber/house-robber.ts
+++ challenges/leetcode/problems/dynamic-programming/house-robber/house-robber.ts
@@ -1,37 +1,33 @@
 /**
- * Leetcode - HouseRobber Recursive approach
+ * Leetcode - HouseRobber bottom up approach
  * recurisve formula:
  * f(0) = array[0]
  * f(1) = Math.max(array[0], array[1])
  * f(i) = Math.max(array[i] + f(i - 2), f(i - 1))
  */
-import { memorized } from '@challenges/util';
 
 function houseRobber(array: number[]): number {
   const n = array.length;
 
-  const houseRobberMemo = memorized(
-    (index: number): number => {
-      if (index < 0) {
-        return 0;
-      }
+  if (n === 0) {
+    return 0;
+  }
 
-      if (index === 0) {
-        return array[0];
-      }
+  if (n === 1) {
+    return array[0];
+  }
 
-      if (index === 1) {
-        return Math.max(array[0], array[1]);
-      }
+  if (n === 2) {
+    return Math.max(array[0], array[1]);
+  }
 
-      return Math.max(
-        array[index] + houseRobberMemo(index - 2),
-        houseRobberMemo(index - 1)
-      );
-    }
-  );
+  const dp: number[] = [array[0], Math.max(array[0], array[1])];
 
-  return houseRobberMemo(n - 1);
+  for (let i = 2; i < n; i = i + 1) {
+    dp[i] = Math.max(array[i] + dp[i - 2], dp[i - 1]);
+  }
+
+  return dp[n - 1];
 }
 
 export { houseRobber };
