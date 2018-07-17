ad778ac09c782447fd3ae839b00eb89a76a6b5f3
somallg <tq.duong@icloud.com>
2018-07-17 09:47:21 +0700

    feat(interviewbit): solved numbers-of-length-n-and-value-less-than-k

--- packages/interviewbit/programming/math/numbers-of-length-n-and-value-less-than-k/numbers-of-length-n-and-value-less-than-k.ts
+++ packages/interviewbit/programming/math/numbers-of-length-n-and-value-less-than-k/numbers-of-length-n-and-value-less-than-k.ts
@@ -2,17 +2,6 @@
  * NumbersOfLengthNAndValueLessThanK
  */
 
-function toDigits(n: number): number[] {
-  const result: number[] = [];
-
-  while (n > 0) {
-    result.push(n % 10);
-    n = Math.floor(n / 10);
-  }
-
-  return result.reverse();
-}
-
 function numbersOfLengthNAndValueLessThanK(
   setOfDigits: number[],
   n: number,
@@ -23,26 +12,33 @@ function numbersOfLengthNAndValueLessThanK(
     return 0;
   }
 
-  const digitsK = toDigits(k);
-  if (digitsK.length < n) {
+  if (k / 10 ** (n - 1) < 1) {
     return 0;
   }
 
   let result = 0;
 
-  for (let i = 0; i < n; i = i + 1) {
+  for (let i = 1; i <= n; i = i + 1) {
     // calculate the possibilty of number having the first
     // digits is digitsC[i]
-    const digitOfK = digitsK[i];
+    const digitOfK = Math.floor(k / 10 ** (n - i));
     const possibleDigits = setOfDigits.filter(digit => {
-      if (i === 0 && n > 1) {
+      if (i === 1 && n > 1) {
         return digit > 0 && digit < digitOfK;
       }
 
       return digit < digitOfK;
     });
 
-    result = result + possibleDigits.length * Math.pow(d, n - i - 1);
+    result = result + possibleDigits.length * Math.pow(d, n - i);
+
+    if (setOfDigits.indexOf(digitOfK) === -1) {
+      // digitOfK not in setOfDigits which mean we can't form
+      // any number starting with digitOfK
+      break;
+    }
+
+    k = k % 10 ** (n - i);
   }
 
   return result;
