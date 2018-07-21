67829c9999c4f4dc2c65a0fe22d975e4e6aa0c56
somallg <tq.duong@icloud.com>
2018-07-21 21:30:59 +0700

    feat(interviewbit): solved search-for-a-range O(logn) time

--- packages/util/array.ts
+++ packages/util/array.ts
@@ -1,3 +1,10 @@
+/*
+ * Utils function which generate an array of size length
+ * Depend on the value of random, if random is true
+ * then it generata an array where each element of the array
+ * in range [startFrom, ...maxExclusive - 1].
+ * If random is false then it return an array [0...0] of size length
+ */
 function generateArray(
   length: number,
   maxExclusive = length,
