3e72c6e93747c227e540888423516120b13dd44c
somallg <tq.duong@icloud.com>
2018-09-12 12:03:52 +0700

    feat(schematics): update template for spec file

--- packages/schematics/challenges/src/file-generator/files/typescript/__problem@dasherize__.spec.ts
+++ packages/schematics/challenges/src/file-generator/files/typescript/__problem@dasherize__.spec.ts
@@ -3,17 +3,17 @@
  * <%= challengeUrl %>
  */
 
-import { createTestExecutor, generateArray, TestFixture } from '@challenges/util';
+import * as ut from '@challenges/util';
 
 import { <%= functionName %> } from './<%= problem %>';
-
 import * as fixture from './<%= problem %>.fixture.json';
-const testFixture = fixture as TestFixture<number, number>;
 
 describe('<%= classify(challengeName) %> - <%= classify(problem) %>', () => {
-  createTestExecutor(testFixture)(
-    <%= functionName %>.name,
-    <%= functionName %>,
-    generateArray
-  );
+  // prettier-ignore
+  ut.createTestExecutor(fixture as ut.TestFixture<number, number>)
+    .executeTests(
+      <%= functionName %>,
+      <%= functionName %>.name,
+      ut.generateArray
+    );
 });
