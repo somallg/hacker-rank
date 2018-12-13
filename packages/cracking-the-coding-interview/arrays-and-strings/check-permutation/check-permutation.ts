f88cd8dfb0ffb1d0d367b12498c14f865a5d3816
somallg <tq.duong@icloud.com>
2018-12-13 11:49:58 +0700

    feat(ctci): add second way to check permutation

--- packages/cracking-the-coding-interview/arrays-and-strings/check-permutation/check-permutation.ts
+++ packages/cracking-the-coding-interview/arrays-and-strings/check-permutation/check-permutation.ts
@@ -2,19 +2,32 @@
  * CheckPermutation
  */
 
-function sortString(s: string): string {
-  return s
-    .split('')
-    .sort()
-    .join();
-}
-
 function checkPermutation(s: string, t: string): boolean {
   if (s.length !== t.length) {
     return false;
   }
 
-  return sortString(s) === sortString(t);
+  const letter: number[] = [];
+
+  s.split('')
+    .map(c => c.charCodeAt(0) - 'a'.charCodeAt(0))
+    .forEach(charCode => {
+      const count = letter[charCode] || 0;
+      letter[charCode] = count + 1;
+    });
+
+  const charcodeOfT = t.split('').map(c => c.charCodeAt(0) - 'a'.charCodeAt(0));
+
+  for (const c of charcodeOfT) {
+    const count = letter[c] || 0;
+    letter[c] = count - 1;
+
+    if (count - 1 < 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 export { checkPermutation };
