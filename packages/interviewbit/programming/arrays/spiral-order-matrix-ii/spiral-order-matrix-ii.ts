0f5ffec2422ba81419ecaac24d993d37136332d1
somallg <tq.duong@icloud.com>
2018-12-26 23:10:31 +0700

    feat(interviewbit): change spiral-order-matrix impl

--- packages/interviewbit/programming/arrays/spiral-order-matrix-ii/spiral-order-matrix-ii.ts
+++ packages/interviewbit/programming/arrays/spiral-order-matrix-ii/spiral-order-matrix-ii.ts
@@ -7,29 +7,38 @@ const RIGHT = 1;
 const DOWN = 2;
 const LEFT = 3;
 
-function getOpFromDirection(direction: number): number[] {
+function canAdvance(
+  [x, y]: number[],
+  direction: number,
+  matrix: number[][]
+): boolean {
+  const [newX, newY] = getNextCoor([x, y], direction);
+
+  if (matrix[newX] === undefined || matrix[newX][newY] === undefined) {
+    return false;
+  }
+
+  return matrix[newX][newY] === 0;
+}
+
+function getNextCoor(
+  [x, y]: [number, number],
+  direction: number
+): [number, number] {
   return {
-    [UP]: [-1, 0],
-    [RIGHT]: [0, 1],
-    [DOWN]: [1, 0],
-    [LEFT]: [0, -1]
+    [RIGHT]: [x, y + 1],
+    [DOWN]: [x + 1, y],
+    [LEFT]: [x, y - 1],
+    [UP]: [x - 1, y]
   }[direction];
 }
 
-function getNextDirection(
-  currentX: number,
-  currentY: number,
-  currentDirection: number,
-  array: number[][]
-): number {
-  const l = array.length;
+function changeDirection(currentDirection: number): number {
   return {
-    [UP]: currentX - 1 < 0 || array[currentX - 1][currentY] !== 0 ? RIGHT : UP,
-    [RIGHT]:
-      currentY + 1 >= l || array[currentX][currentY + 1] !== 0 ? DOWN : RIGHT,
-    [DOWN]:
-      currentX + 1 >= l || array[currentX + 1][currentY] !== 0 ? LEFT : DOWN,
-    [LEFT]: currentY - 1 < 0 || array[currentX][currentY - 1] !== 0 ? UP : LEFT
+    [UP]: RIGHT,
+    [RIGHT]: DOWN,
+    [DOWN]: LEFT,
+    [LEFT]: UP
   }[currentDirection];
 }
 
@@ -40,15 +49,16 @@ function spiralOrderMatrixIi(n: number): number[][] {
   }
 
   let direction: number = RIGHT;
-  let x = 0;
-  let y = 0;
+  let row = 0;
+  let col = 0;
 
   for (let i = 1; i <= n * n; i = i + 1) {
-    result[x][y] = i;
-    direction = getNextDirection(x, y, direction, result);
-    const [opX, opY] = getOpFromDirection(direction);
-    x = x + opX;
-    y = y + opY;
+    result[row][col] = i;
+    if (!canAdvance([row, col], direction, result)) {
+      direction = changeDirection(direction);
+    }
+
+    [row, col] = getNextCoor([row, col], direction);
   }
 
   return result;
