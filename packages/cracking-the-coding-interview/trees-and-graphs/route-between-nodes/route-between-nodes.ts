dc839c4f8ccb83a0943694354bacabd4e568bf9f
somallg <tq.duong@icloud.com>
2019-01-05 15:38:54 +0700

    feat(ctci): fix route-between-nodes

--- packages/cracking-the-coding-interview/trees-and-graphs/route-between-nodes/route-between-nodes.ts
+++ packages/cracking-the-coding-interview/trees-and-graphs/route-between-nodes/route-between-nodes.ts
@@ -16,36 +16,24 @@ function routeBetweenNodes(
 }
 
 function search(graph: Graph, start: Node, end: Node): boolean {
-  // use bfs
-  // 1. push start vao queue
-  // 2. lap den khi queue rong thi thoi
-  // 3. voi moi vang lap while lay Node dau tien trong queue goi la u
-  // 4. neu nhu Node u khong undefined/null di den 5
-  // 5. lay tat ca Node lien ke cua u goi la v
-  // 6. neu v.state !== unvisited to next sang v tiep theo
-  // 6. neu v === end nghia la da co duong den start => end
-  // 7. neu khong add v vao queue va gan v === visiting
-  // 8. sau khi duyet het node lien ke cua u gan u.start = Visited
-  // 9. den vong lap while tiep theo
   if (start === end) {
     return true;
   }
 
   const queue: Node[] = [];
-
   start.state = State.Visiting;
   queue.push(start);
 
   while (queue.length > 0) {
     const u = queue.shift();
 
-    if (u !== undefined) {
+    if (u) {
       for (const v of u.getAdjacent()) {
-        if (v.state !== State.Unvisited) {
+        if (v.state === State.Visited) {
           continue;
         }
 
-        // v is unvisited
+        // v is not visited or visiting
         if (v === end) {
           return true;
         }
